f2c1909760a5aec1708c59fd6e5f8a419fb41833
a.kubrak <padme@bbq.agency>
2022-08-19 22:16:49 +0300

    remove comment

--- src/com/kubrak/helloworld/HelloWorld.java
+++ src/com/kubrak/helloworld/HelloWorld.java
@@ -2,7 +2,6 @@ package com.kubrak.helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello World!");
     }
 }
-//test 12
