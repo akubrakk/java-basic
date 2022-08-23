640620f20986ceef343dfed0788f7dbe67dd7cd1
a.kubrak <padme@bbq.agency>
2022-08-23 17:58:33 +0300

    Fix format

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -4,19 +4,19 @@ import static java.lang.Math.addExact;
 
 public class Main {
 
- public static void printMessage(String s){
-  System.out.println(s);
- }
+    public static void printMessage(String s) {
+        System.out.println(s);
+    }
+
     private static void printSum(int a, int b) {
         System.out.println(addExact(a, b));
     }
 
-   public static void main(String[] args) {
-    String s = "Count of times to write code correctly";
-    printMessage(s);
-    printSum(5,3);
+    public static void main(String[] args) {
+        String s = "Count of times to write code correctly";
+        printMessage(s);
+        printSum(5, 3);
     }
-
 
 
 }
