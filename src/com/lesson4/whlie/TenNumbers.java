44adb400b85fc95df1f4a896664f634365cca7d6
Anastasiia Kubrak <111575681+akubrakk@users.noreply.github.com>
2022-08-29 19:36:34 +0300

    Homework for lesson 4 (#6)

---
+++ src/com/lesson4/whlie/TenNumbers.java
@@ -0,0 +1,16 @@
+package com.lesson4.whlie;
+
+public class TenNumbers {
+    /*
+    Print numbers from 1 to 10
+    using loop while
+     */
+    public static void main(String[] args) {
+
+        int x = 1;
+        while (x <= 10) {
+            System.out.println(x);
+            x++;
+        }
+    }
+}
