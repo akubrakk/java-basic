32ed4eda07b02731d1f17d810244c258d9d6b5d9
Anastasiia Kubrak <111575681+akubrakk@users.noreply.github.com>
2022-09-11 13:53:46 +0300

    Homework lesson7 (#9)
    * Homework lesson7
    
    * fix lesson7

---
+++ src/com/lesson7/Car.java
@@ -0,0 +1,11 @@
+package com.lesson7;
+
+public abstract class Car {
+    protected Engine engine;
+
+    public Car(Engine engine) {
+        this.engine = engine;
+    }
+
+    abstract public void setEngine();
+}
